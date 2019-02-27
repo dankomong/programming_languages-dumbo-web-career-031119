28a2feb141cd248cf7ed351ee3ecafedf86f0c16
Daniel Ko <dankomong@gmail.com>
2019-02-27 23:25:20 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,10 @@
 def reformat_languages(languages)
   # your code here
   new_hash = {}
-  languages.each do |key, value|
-    
+  languages.each do |lang, value|
+    if lang == :ruby 
+      
+    end
+
   end
 end
