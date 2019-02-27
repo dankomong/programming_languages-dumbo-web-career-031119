f0da269f84c9879b5b6241270453044233f103fa
Daniel Ko <dankomong@gmail.com>
2019-02-27 23:45:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, lang|
     lang.each do |spec, info|
-      new_hash[lang] = info
-      new_hash[lang][:style] = []
-      new_hash[lang][:style].push(type)
+      new_hash[spec] = info
+      new_hash[spec][:style] = []
+      new_hash[spec][:style].push(type)
     end
   end
   new_hash
