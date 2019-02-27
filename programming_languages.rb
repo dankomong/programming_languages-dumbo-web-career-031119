ac6d1eaf9d6649bcee9d8f3d09616dce18bd15b5
Daniel Ko <dankomong@gmail.com>
2019-02-27 23:38:36 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,13 +4,6 @@ def reformat_languages(languages)
   languages.each do |type, value|
     type.each do |type, info|
       new_hash[type] = info
-      if type == :ruby || type == :python || type == :java
-        new_hash[type][:style] = [:oo]
-      elsif type == :javascript
-        new_hash[type][:style] = [:oo, :functional]
-      elsif type == :clojure || type == :erlang || type == :scala
-        new_hash[type][:style] = [:functional]
-      end 
     end
   end
   new_hash
